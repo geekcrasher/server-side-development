a8b78dde05a5eafb6f89b3d8092f5324ee22a648
Paulo Lopez <geekcrasher@gmail.com>
2022-11-17 11:45:13 +0800

    added node examples

---
+++ node-examples/index.js
@@ -0,0 +1,32 @@
+
+// * using node module 
+var rect = require("./rectangle")
+
+
+// * Creating object for perimeter and area
+/*
+var rect = {
+   perimeter: (x,y) => (2*(x+y)),
+   area: (x,y) => (x*y)
+}
+*/
+
+function solveRect(l,b) {
+   console.log(`Solving for rectangle with l = ${l} and b = ${b}`)
+
+   if(l<=0 || b<=0) {
+      console.log(`Rectangle dimensions should be greater than zero: l = ${l}, and b = ${b}`)
+   }
+   else {
+      console.log(`The area of the rectangle is ${rect.area(l,b)}`)
+      console.log(`The perimeter of the rectangle is ${rect.perimeter(l,b)}`)
+   }
+}
+
+solveRect(2,4)
+solveRect(3,6)
+solveRect(2,4)
+solveRect(-3,4)
+
+
+
