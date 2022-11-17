fba4c18f87da92d10eaa6a63c366253889e1dc9a
Paulo Lopez <geekcrasher@gmail.com>
2022-11-17 14:46:03 +0800

    node callbacks and error handling

--- node-examples/index.js
+++ node-examples/index.js
@@ -2,9 +2,8 @@
 // * using node module 
 var rect = require("./rectangle")
 
-
-// * Creating object for perimeter and area
 /*
+// * Creating an object for perimeter and area
 var rect = {
    perimeter: (x,y) => (2*(x+y)),
    area: (x,y) => (x*y)
@@ -14,6 +13,17 @@ var rect = {
 function solveRect(l,b) {
    console.log(`Solving for rectangle with l = ${l} and b = ${b}`)
 
+   rect(l,b, (err, rectangle) => {
+      if(err) {
+         console.log("ERROR: ", err.message)
+      }
+      else {
+         console.log(`The area of the rectangle of dimension l = ${l} and b = ${b} is ${rectangle.area()}`)
+         console.log(`The perimeter of the rectangle of dimension l = ${l} and b = ${b} is ${rectangle.perimeter()}`)
+      }
+   })
+
+/*
    if(l<=0 || b<=0) {
       console.log(`Rectangle dimensions should be greater than zero: l = ${l}, and b = ${b}`)
    }
@@ -21,6 +31,7 @@ function solveRect(l,b) {
       console.log(`The area of the rectangle is ${rect.area(l,b)}`)
       console.log(`The perimeter of the rectangle is ${rect.perimeter(l,b)}`)
    }
+*/
 }
 
 solveRect(2,4)
